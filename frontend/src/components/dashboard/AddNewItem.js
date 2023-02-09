4a50f411fdb0c81f0b2c1abaed8ab0bf56eb1f68
PaulKalho <kalhornpaul@gmail.com>
2023-02-09 19:16:51 +0100

    Added PopUp to add a Server
    Styling missing

--- frontend/src/components/dashboard/AddNewItem.js
+++ frontend/src/components/dashboard/AddNewItem.js
@@ -1,9 +1,9 @@
 import React from "react"
 import {BsPlusCircle} from "react-icons/bs";
 
-function AddNewItem (props) {
+function AddNewItem ({onClick}) {
   return (
-    <div className="grid grid-cols-1 grid-rows-auto-1 p-3 h-60 w-1/4 border-dashed border-2 border-white place-content-around rounded-lg text-center">
+    <div onClick={onClick} className="grid grid-cols-1 grid-rows-auto-1 p-3 h-60 w-1/4 border-dashed border-2 border-white place-content-around rounded-lg text-center hover:cursor-pointer">
       <BsPlusCircle className="m-auto" size={130}/>
       <div className="relative text-lg h-10">Add Server</div>
     </div>
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,14 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+import PopUpAddServer from "../components/context/PopUpAddServer";
 import AddNewItem from "../components/dashboard/AddNewItem";
 import Item from "../components/dashboard/Item";
 
 function Dashboard () {
+  const[isPopup, setIsPopup] = useState(false);
+
+  function handlePopup() {
+    setIsPopup(!isPopup);
+  }
+
   return (
     <div>
         <div className="Statistik">Statistik</div>
         <div className="grid-area flex gap-4">
             <Item />
-            <AddNewItem />
+            <AddNewItem onClick={handlePopup}/>
+            { isPopup ? <PopUpAddServer  onClick={handlePopup}/> : <div></div>}
         </div>
     </div>
   )
